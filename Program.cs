383066336e0735b55e76df0cf715d7a43afbcb9d
Philip Tomlinson <phil@philt.dev>
2025-09-04 15:17:24 +0200

    Branchins, ifs, and conditional logic.

--- Program.cs
+++ Program.cs
@@ -6,39 +6,25 @@ public class Program
     {
         #region Strings
 
-        // This line prints "Hello, World!" to the console.
         Console.WriteLine("Hello, World!");
-        
-        // Strings and interpolation.
+
         string firstFriend = "Maria";
         string secondFriend = "Scott";
-
+        
+        Console.WriteLine("My friends are " + firstFriend + " and " + secondFriend);
         Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
 
-        // Concatenation.
-        Console.WriteLine("My friends are " + firstFriend + " and " + secondFriend);
-
-        // String trimming.
         string greeting = "      Greetings!  ";
 
         Console.WriteLine(greeting.Trim());
 
-        // String replacement.
         string friends = $"My friends are {firstFriend} and {secondFriend}";
 
         Console.WriteLine(friends.Replace("Maria", "John"));
-
-        // String contains.
         Console.WriteLine(friends.Contains("Scott"));
         Console.WriteLine(friends.Contains("Rebecca"));
-
-        // String to upper case.
         Console.WriteLine(friends.ToUpper());
-
-        // String length.
         Console.WriteLine(friends.Length);
-
-        // String starts with.
         Console.WriteLine(friends.StartsWith("Scott"));
         Console.WriteLine(friends.StartsWith("My"));
 
@@ -46,19 +32,65 @@ public class Program
 
         #region Numbers
 
-        // Casting to an integer.
         int a = (int)42.1;
         int b = (int)38.3;
         long c = checked(a + b);
 
         Console.WriteLine($"The sum of {a} and {b} is {c}");
 
-        // Decimal numbers.
         decimal d = 42.1M;
         decimal e = 38.3M;
         decimal f = checked(d + e);
 
         Console.WriteLine($"The sum of {d} and {e} is {f}");
+
+        int g = 1;
+        int h = 2;
+        int i = checked(g + h);
+
+        #endregion
+
+        #region Branching, ifs, and conditional logic
+
+        if (d + e > 10)
+        {
+            Console.WriteLine($"The sum of {d} and {e} is greater than 10");
+        }
+        else
+        {
+            Console.WriteLine($"The sum of {d} and {e} is not greater than 10");
+        }
+
+        if (i > 10)
+        {
+            Console.WriteLine($"The sum of {g} and {h} is greater than 10");
+        }
+        else
+        {
+            Console.WriteLine($"The sum of {g} and {h} is not greater than 10");
+        }
+
+        if ((d + e > 10) && (i == 3))
+        {
+            Console.WriteLine($"The sum of {d} and {e} is greater than 10");
+            Console.WriteLine($"And the sum of {g} and {h} is 3");
+        }
+        else
+        {
+            Console.WriteLine($"The sum of {g} and {h} is not greater than 10");
+            Console.WriteLine($"And/or the sum of {g} and {h} is not 3");
+        }
+
+        if ((d + e > 10) || (i == 3))
+        {
+            Console.WriteLine($"Either the sum of {d} and {e} is greater than 10");
+            Console.WriteLine($"Or the sum of {g} and {h} is 3");
+        }
+        else
+        {
+            Console.WriteLine($"The sum of {g} and {h} is not greater than 10");
+            Console.WriteLine($"And the sum of {g} and {h} is not 3");
+        }
 
         #endregion
     }
